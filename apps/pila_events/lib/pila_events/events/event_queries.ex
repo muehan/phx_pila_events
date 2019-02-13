a14390d4d43088b61cc2bc648dd34bfbc7a0bd44
André <tdc@gmx.ch>
2019-02-13 21:16:41 +0100

    validation

--- apps/pila_events/lib/pila_events/events/event_queries.ex
+++ apps/pila_events/lib/pila_events/events/event_queries.ex
@@ -6,4 +6,15 @@ defmodule PilaEvents.EventQueries do
     def get_all do
         Repo.all(from Events)
     end
+
+    def get_all_for_location(location) do
+        query = from e in Events,
+            where: e.location == ^location
+
+        Repo.all(query)
+    end
+
+    def get_by_id(id) do
+        Repo.get(Events, id)
+    end
 end
--- apps/pila_events/lib/pila_events/events/events.ex
+++ apps/pila_events/lib/pila_events/events/events.ex
@@ -1,6 +1,8 @@
 defmodule PilaEvents.Events do
     use Ecto.Schema
 
+    import Ecto.Changeset
+
     schema "events" do
         field :title, :string
         field :location, :string
@@ -9,5 +11,23 @@ defmodule PilaEvents.Events do
 
         timestamps()
     end
+
+    @required_fields ~w(location date)a
+    @optional_fields ~w(description)a
+
+    def changeset(event, params \\ %{}) do
+       event
+       |> cast(params, @required_fields ++ @optional_fields)
+       |> validate_required(@required_fields)
+       |> validate_change(:date, &must_be_future/2)
+    end
+
+    defp must_be_future(_, value) do
+        DateTime.compare(value, DateTime.utc_now())
+        |> get_error
+    end
+
+    defp get_error(comparison) when comparison == :lt, do: [date: "Cannot be in the past"]
+    defp get_error(_), do: []
 
 end
